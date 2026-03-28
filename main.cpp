48c94bacb57ca4515cd9e92e3477512991de4c85
Morloking <dyubenkosergei@yandex.ru>
2026-03-28 07:14:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name{ "" };
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!\n";
+}
+
